6c8316d1f90cf71b1e3271ce00f1c0d45a2a90e4
trillville <tillman.elser@gmail.com>
2016-10-08 21:22:42 -0400

    need to fill in last.id

--- predict_tweets.R
+++ predict_tweets.R
@@ -7,6 +7,8 @@ setup_twitter_oauth(twitter_consumer,
                     twitter_access_token,
                     twitter_access_secret)
 
+last.id <- 
+
 predictions <- predictTweets(last.id)
 
 write.csv(predictions, "predictions.csv")
