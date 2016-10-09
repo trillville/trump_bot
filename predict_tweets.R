b8c315ea2294d2768fc1fd6eb017df8f73bf627c
trillville <tillman.elser@gmail.com>
2016-10-08 21:07:13 -0400

    added prediction functions

---
+++ predict_tweets.R
@@ -0,0 +1,14 @@
+source("libs_constants.R")
+source("functions.R")
+source("keys.R")
+
+load("model.Rdata")
+
+setup_twitter_oauth(twitter_consumer,
+                    twitter_consumer_secret,
+                    twitter_access_token,
+                    twitter_access_secret)
+
+predictions <- predictTweets(last.id)
+
+
